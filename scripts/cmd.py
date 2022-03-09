3ad1b9536856d3547c5ccea51a840eb53bd609ce
Ekky Armandi <ekkyarmandi@gmail.com>
2022-03-09 08:47:26 +0800

    modifying a function into sec to pass

--- scripts/cmd.py
+++ scripts/cmd.py
@@ -1,4 +1,4 @@
-def sec2hms(sec):
+def sec2pass(sec):
     '''
     Converting seconds to Hours and Minute
     :param sec: int -> seconds
@@ -8,6 +8,9 @@ def sec2hms(sec):
     if m <= 60:
         if m == 1: return f"{int(m):d} minute ago"
         else: return f"{int(m):d} minutes ago"
+    elif m > 1440:
+        d, m = divmod(m, 1440)
+        return f"{int(d):d} days and {int(m/60):d} hours ago"
     elif m > 60:
         h, m = divmod(m, 60)
         if m == 1: return f"{int(h):d} hours and {int(m):d} minute ago"
